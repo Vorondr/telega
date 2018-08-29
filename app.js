7ff11b2aa1d33a92c61ba0c925e0fb68206b73c7
Vorondr <vorondrburan@yandex.ru>
2018-08-29 14:38:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const token = '654222218:AAEygkbG2tZsI1iQEe_xnuc6_RDdIwh6wGE';
+const token = '687793330:AAEzBO2L8KDQODv2awfx4IEv00KLzlSmARA';
 const bot = new TelegramBot(token, { polling: true });
-const request = require('request');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 
 bot.onText(/\/start/, function (msg) {
-  var chatId = msg.from.id;
-  bot.sendMessage(chatId, "Привет! Каждый 4 часа я буду отправлять тебе котировки криптовалют по отношению к $$$. Чтобы меня остановить используй /stop");
+  var chatId = "@youcourse";
+  //var timer;
+  //bot.sendMessage(chatId, "Привет! Каждый 4 часа я буду отправлять тебе котировки криптовалют по отношению к $$$.");
   var url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,EOS,DOGE,XMR,DASH,SRP,BCH,NEO&tsyms=USD';
-  this.interval = setInterval(function(){
-    var cryptomsg = [];
+  setInterval(function (){
+  var cryptomsg = [];
       xhr.open('GET', url, false);
       xhr.send();
       if (xhr.status != 200) {
@@ -21,15 +20,10 @@ bot.onText(/\/start/, function (msg) {
         for(var prop in data){
           cryptomsg.push(prop + " : " + data[prop].USD + " $");
         };
-        var stringMsg = JSON.stringify(cryptomsg, null, 4).replace("[","********************").replace("]","********************");
+        var stringMsg = JSON.stringify(cryptomsg, null, 4).replace("[","Курс криптовалюты").replace("]"," ");
         bot.sendMessage(chatId,stringMsg);
-      }   
-    },2000);
+      }
+}, 14400000);
+  
   });
-bot.onText(/\/stop/, function (msg) {
-  var chatId = msg.from.id;
-  clearInterval(this.interval);
-  xhr.abort();
-  bot.sendMessage(chatId, "Хорошо, больше не буду :( Чтобы возобновить используй /start");
-});
 
